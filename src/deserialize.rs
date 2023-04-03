fb6c0149d0901195f9963a8b0c5671d16cfa1506
Stefan Bossbaly <sbossb@gmail.com>
2023-04-02 20:55:54 -0400

    Added deserialize_string_enum
    Added the ability to deserialize string into enum for all enums that
    implement the FromStr trait.

--- src/deserialize.rs
+++ src/deserialize.rs
@@ -1,10 +1,18 @@
 use serde::de;
-use std::fmt;
+use std::{fmt, str::FromStr};
 
 pub fn deserialize_csv_encoded_string<'a, D: de::Deserializer<'a>>(
     deserializer: D,
 ) -> Result<Vec<i32>, D::Error> {
     deserializer.deserialize_str(CsvEncodedStringVisitor)
+}
+
+pub fn deserialize_string_enum<'a, D: de::Deserializer<'a>, T: FromStr + 'a>(
+    deserializer: D,
+) -> Result<T, D::Error> {
+    deserializer.deserialize_str(StringEnumVisitor {
+        _marker: std::marker::PhantomData,
+    })
 }
 
 struct CsvEncodedStringVisitor;
@@ -29,5 +37,26 @@ impl<'a> de::Visitor<'a> for CsvEncodedStringVisitor {
         }
 
         Ok(result)
+    }
+}
+
+#[derive(Default)]
+struct StringEnumVisitor<'a, T: FromStr> {
+    _marker: std::marker::PhantomData<&'a T>,
+}
+
+impl<'a, T: FromStr> de::Visitor<'a> for StringEnumVisitor<'a, T> {
+    type Value = T;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "a string representation an enum value")
+    }
+
+    fn visit_str<E: de::Error>(self, v: &str) -> Result<Self::Value, E> {
+        let enum_value: T = v
+            .parse()
+            .map_err(|_| de::Error::custom(format!("invalid value {}", v)))?;
+
+        Ok(enum_value)
     }
 }
