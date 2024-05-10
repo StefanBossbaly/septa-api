07fc90bbc8df8267dff833ec59316a2d9f5fe73f
Stefan Bossbaly <sbossb@gmail.com>
2024-05-10 00:11:15 -0400

    Use thiserror

--- src/errors.rs
+++ src/errors.rs
@@ -1,42 +1,14 @@
-use std::fmt;
-
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
-    RequestFailed(reqwest::Error),
-    DeserializeFailed(serde_json::error::Error),
+    #[error("Unable to send the request: {0}")]
+    RequestFailed(#[from] reqwest::Error),
+
+    #[error("Unable to deserialize the received value: {0}")]
+    DeserializeFailed(#[from] serde_json::error::Error),
+
+    #[error("API returned an error response: {0}")]
     ApiErrorResponse(String),
+
+    #[error("Unknown regional rail station: {0}")]
     UnknownRegionalRailStation(String),
-}
-
-impl std::error::Error for Error {}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::RequestFailed(e) => {
-                write!(f, "Unable to send the request: {}", e)
-            }
-            Self::DeserializeFailed(e) => {
-                write!(f, "Unable to deserialize the received value: {}", e)
-            }
-            Self::ApiErrorResponse(e) => {
-                write!(f, "API returned an error response: {}", e)
-            }
-            Self::UnknownRegionalRailStation(e) => {
-                write!(f, "Unknown regional rail station: {}", e)
-            }
-        }
-    }
-}
-
-impl From<serde_json::error::Error> for Error {
-    fn from(error: serde_json::error::Error) -> Self {
-        Self::DeserializeFailed(error)
-    }
-}
-
-impl From<reqwest::Error> for Error {
-    fn from(error: reqwest::Error) -> Self {
-        Self::RequestFailed(error)
-    }
 }
